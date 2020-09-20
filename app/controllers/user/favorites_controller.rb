32e65cd2e65fefb79886dc7ffbc0babbfe576ebe
vagrant <akzouksia@icloud.com>
2020-09-20 19:20:10 +0900

    [update]ajax favorite

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -4,13 +4,15 @@ class User::FavoritesController < ApplicationController
 		@post = Post.find(params[:post_id])
 		favorite = current_user.favorites.new(post_id: @post.id)
 		favorite.save
-		redirect_back(fallback_location: root_path)
+		@event = Event.find(params[:event_id])
+		@post = Post.find(params[:post_id])
 	end
 
 	def destroy
 		@post = Post.find(params[:post_id])
 		favorite = current_user.favorites.find_by(post_id: @post.id)
 		favorite.destroy
-		redirect_back(fallback_location: root_path)
+		@event = Event.find(params[:event_id])
+		@post = Post.find(params[:post_id])
 	end
 end
