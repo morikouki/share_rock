5e59e3b7c7b4a281d714f8a5f8bd6ad83b1910ec
vagrant <akzouksia@icloud.com>
2020-09-20 18:30:08 +0900

    [update]comment_favorite

--- app/controllers/user/comment_favorites_controller.rb
+++ app/controllers/user/comment_favorites_controller.rb
@@ -2,20 +2,16 @@ class User::CommentFavoritesController < ApplicationController
 
 	def create
 		@post_comment = PostComment.find(params[:post_comment_id])
-		post = Post.find(params[:post_id])
-		comment_favorite = current_user.comment_favorites.new(params[post_comment_id: @post_comment.id])
-		comment_favorite.post_id = post.id
-		comment_favorite.post_comment_id = @post_comment.id
+		@post = Post.find(params[:post_id])
+		comment_favorite = current_user.comment_favorites.new(post_comment_id: @post_comment.id, post_id: @post.id)
 		comment_favorite.save
 		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 		@post_comment = PostComment.find(params[:post_comment_id])
-		post = Post.find(params[:post_id])
-		comment_favorite = current_user.comment_favorites.find_by(params[post_comment_id: @post_comment.id])
-		comment_favorite.post_id = post.id
-		comment_favorite.post_comment_id = @post_comment.id
+		@post = Post.find(params[:post_id])
+		comment_favorite = current_user.comment_favorites.find_by(post_comment_id: @post_comment.id, post_id: @post.id)
 		comment_favorite.destroy
 		redirect_back(fallback_location: root_path)
 	end
