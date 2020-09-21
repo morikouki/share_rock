0715136f5944e6576a3f6f93cdf9ca457e003cbf
vagrant <akzouksia@icloud.com>
2020-09-21 12:56:00 +0900

    [update]comment_favorite ajax

--- app/controllers/user/comment_favorites_controller.rb
+++ app/controllers/user/comment_favorites_controller.rb
@@ -3,16 +3,16 @@ class User::CommentFavoritesController < ApplicationController
 	def create
 		@post_comment = PostComment.find(params[:post_comment_id])
 		@post = Post.find(params[:post_id])
+		@event = Event.find(params[:event_id])
 		comment_favorite = current_user.comment_favorites.new(post_comment_id: @post_comment.id, post_id: @post.id)
 		comment_favorite.save
-		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 		@post_comment = PostComment.find(params[:post_comment_id])
 		@post = Post.find(params[:post_id])
+		@event = Event.find(params[:event_id])
 		comment_favorite = current_user.comment_favorites.find_by(post_comment_id: @post_comment.id, post_id: @post.id)
 		comment_favorite.destroy
-		redirect_back(fallback_location: root_path)
 	end
 end
--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -2,17 +2,15 @@ class User::FavoritesController < ApplicationController
 
 	def create
 		@post = Post.find(params[:post_id])
+		@event = Event.find(params[:event_id])
 		favorite = current_user.favorites.new(post_id: @post.id)
 		favorite.save
-		@event = Event.find(params[:event_id])
-		@post = Post.find(params[:post_id])
 	end
 
 	def destroy
 		@post = Post.find(params[:post_id])
+		@event = Event.find(params[:event_id])
 		favorite = current_user.favorites.find_by(post_id: @post.id)
 		favorite.destroy
-		@event = Event.find(params[:event_id])
-		@post = Post.find(params[:post_id])
 	end
 end
