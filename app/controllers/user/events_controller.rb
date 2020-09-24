ec666783716716331a4ed1933ad71fa43ec0ee78
vagrant <akzouksia@icloud.com>
2020-09-24 17:26:24 +0900

    [update]event_index_view

--- app/controllers/user/events_controller.rb
+++ app/controllers/user/events_controller.rb
@@ -1,17 +1,16 @@
 class User::EventsController < ApplicationController
 
 	def index
-		day = Date.start_date
-		day.year
+		
 		case params[:year]
 		when '0'
-			@events = Event.where(day: 2020)
+			@events = Event.where("start_date LIKE?", "%2020%")
 		when '1'
-			@events = Event.where(day: 2019)
+			@events = Event.where("start_date LIKE?", "%2019%")
 		when '2'
-			@events = Event.where(start_date: '2018')
+			@events = Event.where("start_date LIKE?", "%2018%")
 		when '3'
-			@events = Event.where(start_date: '2017')
+			@events = Event.where("start_date LIKE?", "%2017%")
 		else
 		    @events = Event.all
 		end
