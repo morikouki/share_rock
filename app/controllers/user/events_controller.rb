2bcfe86d8df49c78bd9be3d706092ccfd7d67f50
vagrant <akzouksia@icloud.com>
2020-09-24 16:51:23 +0900

    [update]events_controller

--- app/controllers/user/events_controller.rb
+++ app/controllers/user/events_controller.rb
@@ -1,7 +1,20 @@
 class User::EventsController < ApplicationController
 
 	def index
-		@events = Event.all
+		day = Date.start_date
+		day.year
+		case params[:year]
+		when '0'
+			@events = Event.where(day: 2020)
+		when '1'
+			@events = Event.where(day: 2019)
+		when '2'
+			@events = Event.where(start_date: '2018')
+		when '3'
+			@events = Event.where(start_date: '2017')
+		else
+		    @events = Event.all
+		end
 	end
 
 	def show
